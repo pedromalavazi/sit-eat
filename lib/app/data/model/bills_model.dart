3a387073f1310da2171018b1d30729aeb10ceab0
Pedro Malavazi <phmalavazi@hotmail.com>
2021-11-18 23:06:13 -0300

    ajustes payment

--- lib/app/data/model/bills_model.dart
+++ lib/app/data/model/bills_model.dart
@@ -6,17 +6,32 @@ class BillModel {
   bool asked;
   bool paid;
   double total;
+  String paymentType;
 
-  BillModel({this.id, this.reservationId, this.asked, this.paid, this.total});
+  BillModel(
+      {this.id,
+      this.reservationId,
+      this.asked,
+      this.paid,
+      this.total,
+      this.paymentType});
 
   BillModel.fromSnapshot(DocumentSnapshot bill)
       : id = bill.id,
         reservationId = bill.data()["reservationId"],
         asked = bill.data()["asked"],
         paid = bill.data()["paid"],
-        total = bill.data()["total"];
+        total = bill.data()["total"],
+        paymentType = bill.data()["paymentType"];
 
   Map<String, dynamic> toJson() {
-    return {"id": id, "reservationId": reservationId, "asked": asked, "paid": paid, "total": total};
+    return {
+      "id": id,
+      "reservationId": reservationId,
+      "asked": asked,
+      "paid": paid,
+      "total": total,
+      "paymentType": paymentType
+    };
   }
 }
--- lib/app/ui/android/menu/payment_methods_page.dart
+++ lib/app/ui/android/menu/payment_methods_page.dart
@@ -2,13 +2,17 @@ import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:sit_eat/app/controller/reservation_controller.dart';
 import 'package:sit_eat/app/data/services/util_service.dart';
-import 'package:sit_eat/app/routes/app_pages.dart';
 
 class PaymentsPage extends GetView<ReservationController> {
-  final ReservationController _reservationController = Get.find<ReservationController>();
+  final ReservationController _reservationController =
+      Get.find<ReservationController>();
   final UtilService _util = UtilService();
 
-  final List<Item> items = <Item>[Item('Dinheiro'), Item('Cartão'), Item('Vale Refeição')];
+  final List<Item> items = <Item>[
+    Item('Dinheiro'),
+    Item('Cartão'),
+    Item('Vale Refeição')
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -18,80 +22,106 @@ class PaymentsPage extends GetView<ReservationController> {
         centerTitle: true,
       ),
       backgroundColor: Colors.white,
-      body: ListView(
-        padding: EdgeInsets.symmetric(vertical: 100, horizontal: 50),
-        children: List.generate(
-          items.length,
-          (index) {
-            return Obx(
-              () => ListTile(
-                onTap: () {
-                  resetItems(index);
-                  items[index].selected.value = !items[index].selected.value;
+      body: _reservationController.billAsked.value
+          ? Center(
+              child: Text(
+                "Pedido de conta já realizado",
+                style: TextStyle(fontSize: 15),
+              ),
+            )
+          : ListView(
+              padding: EdgeInsets.symmetric(vertical: 100, horizontal: 50),
+              children: List.generate(
+                items.length,
+                (index) {
+                  return Obx(
+                    () => ListTile(
+                      onTap: () {
+                        resetItems(index);
+                        items[index].selected.value =
+                            !items[index].selected.value;
+                        _reservationController.paymentType.value =
+                            items[index].title;
+                      },
+                      selected: items[index].selected.value,
+                      leading: GestureDetector(
+                        behavior: HitTestBehavior.opaque,
+                        child: Container(
+                          width: 48,
+                          height: 48,
+                        ),
+                      ),
+                      selectedTileColor: Colors.grey,
+                      title: Text(
+                        items[index].title,
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black54),
+                      ),
+                      contentPadding: EdgeInsets.only(right: 50),
+                      trailing: (items[index].selected.value)
+                          ? Icon(
+                              Icons.radio_button_checked,
+                              color: Colors.red[500],
+                            )
+                          : Icon(Icons.radio_button_off),
+                    ),
+                  );
                 },
-                selected: items[index].selected.value,
-                leading: GestureDetector(
-                  behavior: HitTestBehavior.opaque,
-                  child: Container(
-                    width: 48,
-                    height: 48,
-                  ),
-                ),
-                title: Text(
-                  items[index].title,
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black54),
-                ),
-                contentPadding: EdgeInsets.only(right: 50),
-                trailing: (items[index].selected.value)
-                    ? Icon(
-                        Icons.radio_button_checked,
-                        color: Colors.red[500],
-                      )
-                    : Icon(Icons.radio_button_off),
               ),
-            );
-          },
-        ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: EdgeInsets.all(40.0),
-        child: ElevatedButton(
-          onPressed: () {
-            showDialog(
-              context: context,
-              builder: (ctx) => AlertDialog(
-                title: Text("Fechamento de conta"),
-                content: Text("Tem certeza que deseja fechar a conta?"),
-                actions: [
-                  ElevatedButton(
-                      style: ElevatedButton.styleFrom(primary: Colors.red[500], padding: EdgeInsets.all(15)),
-                      onPressed: () {
-                        Navigator.of(ctx).pop(false);
-                      },
-                      child: Text("Não")),
-                  ElevatedButton(
-                      style: ElevatedButton.styleFrom(primary: Colors.red[500], padding: EdgeInsets.all(15)),
-                      onPressed: () async {
-                        Navigator.of(ctx).pop(false);
-                        Get.offNamed(Routes.NAVIGATION);
-                        var bill = await _reservationController.getUserBill();
-                        _reservationController.askBill(bill);
-                        _util.showErrorMessage("Solicitação de fechamento", "Sua conta está a caminho!");
-                      },
-                      child: Text("Sim")),
-                ],
+            ),
+      bottomNavigationBar: _reservationController.billAsked.value
+          ? Container()
+          : Padding(
+              padding: EdgeInsets.all(40.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  try {
+                    showDialog(
+                      context: context,
+                      builder: (ctx) => AlertDialog(
+                        title: Text("Fechamento de conta"),
+                        content: Text("Tem certeza que deseja fechar a conta?"),
+                        actions: [
+                          ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  primary: Colors.red[500],
+                                  padding: EdgeInsets.all(15)),
+                              onPressed: () {
+                                Navigator.of(ctx).pop(false);
+                              },
+                              child: Text("Não")),
+                          ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  primary: Colors.red[500],
+                                  padding: EdgeInsets.all(15)),
+                              onPressed: () {
+                                Navigator.of(ctx).pop(false);
+                                _reservationController.askBill();
+                              },
+                              child: Text("Sim")),
+                        ],
+                      ),
+                    );
+                  } catch (e) {
+                    _util.showErrorMessage(
+                        "Erro", "Selecione uma forma de pagamento.");
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.red[500],
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                    textStyle:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Finalizar'),
+                  ],
+                ),
               ),
-            );
-          },
-          style: ElevatedButton.styleFrom(primary: Colors.red[500], padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20), textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Finalizar'),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 
